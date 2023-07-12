a25ddc89a7b652445ed0ad9675e215187a37e420
aIvanml <117483692+aIvanml@users.noreply.github.com>
2023-07-12 16:55:23 -0300

    Dos botones

--- src/App.js
+++ src/App.js
@@ -6,13 +6,18 @@ export default function App() {
   const [clicks, setClicks] = useState(0);
 
   function handleClick() {
-    setClicks(clicks+1)
+    setClicks(clicks + 1);
     //setClicks( estado => estado + 1)
   }
 
   return (
-    <button onClick={handleClick} style={{ border: 'solid 3px', padding: '20px' }}>
-      <h1>Contador de clicks: {clicks}</h1>
-    </button>
+    <div>
+      <button onClick={handleClick} style={{ border: 'solid 3px', padding: '20px' }}>
+        <h1>Sumar: {clicks}</h1>
+      </button>
+      <button onClick={handleClick} style={{ border: 'solid 3px', padding: '20px', marginLeft: '10px' }}>
+        <h1>Restar: {clicks}</h1>
+      </button>
+    </div>
   );
 }
